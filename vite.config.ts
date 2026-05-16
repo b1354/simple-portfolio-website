ad797f43e530e638bcd77a920b87f46d37ccf14f
bayurizky <bayurizkyseptiansyah@gmail.com>
2026-05-16 12:56:48 +0700

    wip: old device issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import legacy from "@vitejs/plugin-legacy"
 import tailwindcss from '@tailwindcss/vite'
 import path from "path"
 
 export default defineConfig({
+  build: {
+    target: "es2015", // not "esnext" — esnext breaks MI Browser
+  },
   plugins: [
     react(),
+     legacy({
+      targets: ["Android >= 6", "Chrome >= 70"],
+      additionalLegacyPolyfills: ["regenerator-runtime/runtime"],
+      renderLegacyChunks: true,
+      modernPolyfills: true,
+    }),
     tailwindcss(),
   ],
   resolve: {
